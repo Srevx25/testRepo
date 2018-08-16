efd92854a433e59579f7a979ffaabe104dfa39ef
Srevx25 <41642653+Srevx25@users.noreply.github.com>
2018-08-16 17:22:20 +0800

    PROG_EXER4

---
+++ PROGRAMMING EXERCISES/PROG_EXER4.cpp
@@ -0,0 +1,19 @@
+/*
+*AUTHOR <Sofia Ruth E. Villanueva>
+*DATE <August 04 2018>
+*NAME <Bags of Rice>
+*DESCRIPTION <>
+*/
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int A=2205;
+	float C,B;
+	cout << "enter the size of the bag:";
+	cin >> C;
+	
+	 B=(A/C);
+	 
+	cout << B;
+}
